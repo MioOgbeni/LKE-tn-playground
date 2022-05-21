7a0071319b4d8281bf7548e89806ced077a66e3a
MioOgbeni <tom.nov96@gmail.com>
2022-05-21 19:01:51 +0200

    Create plain LKE cluster

---
+++ main.tf
@@ -0,0 +1,28 @@
+terraform {
+    required_providers {
+        linode = {
+        source = "linode/linode"
+        version = "1.27.2"
+        }
+    }
+}
+
+provider "linode" {
+    # Configuration options
+}
+
+resource "linode_lke_cluster" "tn_playground_cluster" {
+    label       = "tn-playground"
+    k8s_version = "1.23"
+    region      = "eu-central"
+
+    pool {
+        type  = "g6-standard-1"
+        count = 3
+
+        autoscaler {
+            min = 3
+            max = 6
+        }
+    }
+}
