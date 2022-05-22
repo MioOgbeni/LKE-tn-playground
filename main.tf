a6dbee3f56cde2980948475923770ac34a07642a
MioOgbeni <tom.nov96@gmail.com>
2022-05-22 14:01:42 +0200

    Fix tf formating

--- main.tf
+++ main.tf
@@ -1,36 +1,36 @@
 terraform {
-    cloud {
-        organization = "mioogbeni"
+  cloud {
+    organization = "mioogbeni"
 
-        workspaces {
-            name = "tn-playground"
-        }
+    workspaces {
+      name = "tn-playground"
     }
+  }
 
-    required_providers {
-        linode = {
-            source = "linode/linode"
-            version = "1.27.2"
-        }
+  required_providers {
+    linode = {
+      source  = "linode/linode"
+      version = "1.27.2"
     }
+  }
 }
 
 provider "linode" {
-    # Configuration options
+  # Configuration options
 }
 
 resource "linode_lke_cluster" "tn_playground_cluster" {
-    label       = "tn-playground"
-    k8s_version = "1.23"
-    region      = "eu-central"
+  label       = "tn-playground"
+  k8s_version = "1.23"
+  region      = "eu-central"
 
-    pool {
-        type  = "g6-standard-1"
-        count = 3
+  pool {
+    type  = "g6-standard-1"
+    count = 3
 
-        autoscaler {
-            min = 3
-            max = 6
-        }
+    autoscaler {
+      min = 3
+      max = 6
     }
+  }
 }
